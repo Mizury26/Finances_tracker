6c371db846c5cce6ef2a82b56d1b9332bbc9119b
LeoMa33 <leo.mace@ynov.com>
2025-06-03 16:00:58 +0200

    Auto stash before rebase of "devLeo" onto "dev"

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,19 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.12")
     implementation("log4j:log4j:1.2.17")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testImplementation("org.mockito:mockito-core:5.8.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.8.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
 tasks.withType<Test> {
